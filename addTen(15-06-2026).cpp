88414119f48069a0fbda15a30888ffecfab2bb6e
DHANASHREE VAISHAMPAYAN <dhanashree2812@gmail.com>
2026-06-15 19:08:20 +0530

    Add addTen(15-06-2026).cpp

---
+++ addTen(15-06-2026).cpp
@@ -0,0 +1,21 @@
+#Write a function to add 10 to a number using call by value. Check whether the original number changes.
+#include<iostream>
+using namespace std;
+int add10(int x)
+{
+    x=x+10;
+    cout<<x<<endl;
+    return x;
+    
+}
+int main()
+{
+    int a;
+    cout<<"enter a number:";
+    cin>>a;
+    cout<<"before function call:"<<a<<endl;
+    add10(a);
+    cout<<"after function call:"<<a;
+    
+}
+
