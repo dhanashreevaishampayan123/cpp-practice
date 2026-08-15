ff4fd9450d45217526b9c3298d867ed33c90e22d
DHANASHREE VAISHAMPAYAN <dhanashree2812@gmail.com>
2026-08-15 20:13:30 +0530

    Add files via upload

---
+++ print_data_linked_list.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node *next;
+};
+int main() {
+    Node *first=new Node;
+    Node *second=new  Node;
+    Node *third=new Node;
+    first->data=10;
+    second->data=20;
+    third->data=30;
+    first->next=second;
+    second->next=third;
+    third->next=nullptr;
+    Node *temp=first;
+    while(temp!=nullptr)
+    {
+        cout<<temp->data<<"\n";
+        temp=temp->next;
+    }
+}
