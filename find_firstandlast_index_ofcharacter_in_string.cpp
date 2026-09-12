3369e749f82bd3d80a04c7761f28d770ff782447
DHANASHREE VAISHAMPAYAN <dhanashree2812@gmail.com>
2026-09-12 16:02:16 +0530

    Add files via upload

---
+++ find_firstandlast_index_ofcharacter_in_string.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void findfirstandlastindex(string s,char ch, int *first, int *last)
+{
+    for(int i=0;i<s.size()-1;i++)
+        {
+            if(s[i]==ch)
+            {
+                *first=i;
+                break;
+            }
+        }
+    for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i]==ch)
+            {
+                *last=i;
+                break;
+            }
+        }
+}
+int main() {
+    string s="abcdeba";
+    char ch='a';
+    int first=-1;
+    int last=-1;
+    int *pf=&first;
+    int *pl=&last;
+
+    findfirstandlastindex(s,ch,pf,pl);
+    cout<<first<<" "<<last;
+    
+    return 0;
+}
